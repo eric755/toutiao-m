8b586b43387563045be88b60916d9cf392e71246
lwm <2457894285@qq.com>
2021-03-01 13:47:38 +0800

    项目初始化-引入vant组件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <!-- 路由的出口 -->
-    <router-view/>
+    <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-guanbi"></i>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+      <van-button type="default">默认按钮</van-button>
+      <van-button type="warning">警告按钮</van-button>
+      <van-button type="danger">危险按钮</van-button>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'App'// 建议给组件都取一个名
+  name: 'App' // 建议给组件都取一个名
 }
 </script>
 <style lang="less"></style>
